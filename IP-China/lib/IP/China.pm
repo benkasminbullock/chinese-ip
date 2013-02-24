4e63c33b131da2a3914a82aded31f979971a55e8
Ben Bullock <benkasminbullock@gmail.com>
2013-02-24 14:03:56 +0900

    Version up for next release.

--- IP-China/lib/IP/China.pm
+++ IP-China/lib/IP/China.pm
@@ -4,7 +4,7 @@ require Exporter;
 @EXPORT_OK = qw/chinese_ip/;
 use warnings;
 use strict;
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 require XSLoader;
 XSLoader::load ('IP::China', $VERSION);
 1;
