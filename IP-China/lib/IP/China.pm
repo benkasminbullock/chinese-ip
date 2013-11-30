8f9f1f1cab89ebfc7a59cbf0fca382e70199378b
Ben Bullock <benkasminbullock@gmail.com>
2013-11-30 11:04:03 +0900

    Use version numbers based on the data, rewrite Changes to conform.

--- IP-China/lib/IP/China.pm
+++ IP-China/lib/IP/China.pm
@@ -4,7 +4,7 @@ require Exporter;
 @EXPORT_OK = qw/chinese_ip/;
 use warnings;
 use strict;
-our $VERSION = 0.06;
+our $VERSION = '20131106';
 use IP::Tools;
 require XSLoader;
 XSLoader::load ('IP::China', $VERSION);
