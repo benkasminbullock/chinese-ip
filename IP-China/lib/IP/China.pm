48d0be5d2bed0b3239e32ef7c548a33129a57aac
Ben Bullock <benkasminbullock@gmail.com>
2016-04-27 10:03:25 +0900

    Add distro tests

--- IP-China/lib/IP/China.pm
+++ IP-China/lib/IP/China.pm
@@ -1,9 +1,9 @@
 package IP::China;
-require Exporter;
-@ISA = qw(Exporter);
-@EXPORT_OK = qw/chinese_ip/;
 use warnings;
 use strict;
+require Exporter;
+use base qw(Exporter);
+our @EXPORT_OK = qw/chinese_ip/;
 our $VERSION = '20160412';
 require XSLoader;
 XSLoader::load ('IP::China', $VERSION);
